bd8a41c45de00ae211d87f409598728eca016c05
Wilson Hamilton <wilson@tripshock.com>
2021-09-01 11:47:21 -0400

    Adding load config path static method

--- src/ServerConfig.ts
+++ src/ServerConfig.ts
@@ -4,7 +4,7 @@ export class ServerConfig {
     constructor(){
     }
 
-    loadConfigPath(){
+    static loadConfigPath(){
         return path.resolve(__dirname, '../server-config.json');
     }
 
