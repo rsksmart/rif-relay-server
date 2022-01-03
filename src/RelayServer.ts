150c5829e7d59ea66e6f352763b495842d35e11d
mortelli <ortellimarcelo@gmail.com>
2022-01-03 20:21:32 -0300

    update debug logs

--- src/RelayServer.ts
+++ src/RelayServer.ts
@@ -440,6 +440,7 @@ export class RelayServer extends EventEmitter {
             creationBlockNumber: currentBlock,
             gasPrice: req.relayRequest.relayData.gasPrice
         };
+        log.debug("details is:", JSON.stringify(details));
         const txDetails = await this.transactionManager.sendTransaction(
             details
         );
