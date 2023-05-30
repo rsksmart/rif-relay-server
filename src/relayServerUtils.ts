3b556093ff3af4dac3ebc835a49f89111ab7a0a7
Antonio Morrone <antonio@iovlabs.org>
2023-05-30 15:54:57 +0200

    fix: change the log shown when the request is expired or about to expire

--- src/relayServerUtils.ts
+++ src/relayServerUtils.ts
@@ -10,7 +10,7 @@ import { BigNumber as BigNumberJs } from 'bignumber.js';
 import type { BigNumber, BigNumberish } from 'ethers';
 import { MAX_ESTIMATED_GAS_DEVIATION } from './definitions/server.const';
 import { getProvider } from './Utils';
-import { ERC20__factory } from '@rsksmart/rif-relay-contracts';
+import { ERC20__factory, PromiseOrValue } from '@rsksmart/rif-relay-contracts';
 import type ExchangeToken from './definitions/token.type';
 import {
   convertGasToNative,
@@ -332,6 +332,30 @@ async function convertGasToTokenAndNative(
   };
 }
 
+function secondsToDate(dateInSeconds: number) {
+  return new Date(dateInSeconds * 1000);
+}
+
+async function validateExpirationTime(
+  validUntilTime: PromiseOrValue<BigNumberish>,
+  requestMinValidSeconds: number
+) {
+  const validUntilTimeValue = await validUntilTime;
+  const secondsNow = Math.round(Date.now() / 1000);
+  const expiredInSeconds =
+    parseInt(validUntilTimeValue.toString()) - secondsNow;
+  if (expiredInSeconds < requestMinValidSeconds) {
+    const expirationDate = secondsToDate(
+      parseInt(validUntilTimeValue.toString())
+    );
+    throw new Error(
+      `Request expired (or too close): expiration date received "${expirationDate.toUTCString()}" is expected to be greater than or equal to "${secondsToDate(
+        secondsNow + requestMinValidSeconds
+      ).toUTCString()}"`
+    );
+  }
+}
+
 export {
   validateIfGasAmountIsAcceptable,
   validateIfTokenAmountIsAcceptable,
@@ -340,4 +364,5 @@ export {
   isSponsorshipAllowed,
   TRANSFER_HASH,
   TRANSFER_FROM_HASH,
+  validateExpirationTime,
 };
